b300954131e5cdad2f4ecd9d0c46e288e6e15919
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-08-25 15:58:37 +0300

    Added Task010

---
+++ Homework/Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrintSecondNumber ()
+{
+Console.Write("Введите трехзначное и нажмите клавишу Enter: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10;
+b = b % 10;
+
+Console.WriteLine(b);
+}
+
+PrintSecondNumber (); 
+
