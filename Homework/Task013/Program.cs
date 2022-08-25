a8b163632768e1040ca95e37ae7bf804023bf13f
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-08-25 16:17:49 +0300

    Added Task013

---
+++ Homework/Task013/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите и нажмите клавишу Enter: ");
+
+string arr = Console.ReadLine();
+int len = arr.Length;
+int n = Convert.ToInt32(arr);
+int i = 0;
+
+if (len <= 2)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    while (i < (len - 3))
+
+    {
+        n = n / 10;
+        i++;
+    }
+    Console.WriteLine(n % 10);
+}
