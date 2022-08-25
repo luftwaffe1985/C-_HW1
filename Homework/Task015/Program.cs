5e4c478d2af3032abdae018356b8d7e54028382d
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-08-25 16:29:42 +0300

    Added Task015

---
+++ Homework/Task015/Program.cs
@@ -0,0 +1,22 @@
+﻿void CheckWeekend ()
+{
+Console.Write("Введите номер дня недели и нажмите клавишу Enter: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 5 & a < 8)
+{
+    Console.WriteLine("Выходной");
+}
+else if (a >= 1 & a <= 5 )
+{
+    Console.WriteLine("Рабочий");
+}
+else
+{
+    Console.WriteLine("В неделе 7 дней!");
+}
+}
+
+CheckWeekend ();
+CheckWeekend ();
