f37098bf1f5768b07c76e9a56e9bacf8cff09866
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-08-30 17:42:28 +0300

    Added Task023

---
+++ Homework/Task023/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+string getPower3 ()
+{
+Console.Write("Введите положительное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int power = 3;
+string res = string.Empty;  // Пустая строка
+while (i <= N)
+    {
+        res += Math.Pow(i, power) + ", ";
+        i++;
+                 
+    }
+return res;
+}
+try
+{
+    Console.WriteLine(getPower3 ());
+}
+catch
+{
+    Console.WriteLine("необходимо ввести положительное натуральное число цифрой");
+}
+
