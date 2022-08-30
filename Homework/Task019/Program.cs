8102d629c7a8e53abcbb6e2d000cdb0229d8fc4f
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-08-30 16:06:10 +0300

    Added Task019

---
+++ Homework/Task019/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string CheckPalindrome ()
+{
+Console.Write("Введе число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number;
+int result = 0;
+        
+    while (number1 > 0) 
+    {
+    result *= 10;
+    result += number1 % 10;
+    number1 /= 10;
+    }
+    if (number == result)
+    {
+        return $"Число {number} является палиндромом";
+    }
+    else
+    {
+        return $"Число {number} не является палиндромом";
+    }
+}
+try
+{
+    Console.WriteLine(CheckPalindrome ());
+}
+catch
+{
+    Console.WriteLine("необходимо ввести целочисленное значение цифрами");
+}
+
