315b84d2a7fb037fe2c03d43532a2078f9bc6624
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-09-04 15:23:18 +0300

    Added Task025

---
+++ Homework/Task025/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int aToBPower(int numA, int numB)
+{
+int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result *= numA;
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int exponent = aToBPower(numA, numB);
+Console.WriteLine("Ответ: " + exponent);
+
+
